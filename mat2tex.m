2a79a1275b5e222af8ef277f8d027767264eac78
arkaragian <arkaragian@gmail.com>
2014-02-19 03:34:01 +0000

    Update of the initial mat2tex function
    printNumber.m function is now an internal function in mat2tex.m

--- mat2tex.m
+++ mat2tex.m
@@ -1,4 +1,4 @@
-A=rand(5,10);
+function mat2tex(A)
 
 [rows,cols] = size(A);
 
@@ -13,3 +13,36 @@ for i=1:1:rows
     fprintf('\n');
 end
 
+%Internal function
+function printNumber(number,endline)
+	%Compare vapriable type string
+	vartype = class(endline);
+	if(~strcmp(vartype,'logical'))
+		error('endline Variable must be boolean!');
+	end
+
+	%Compare number vapriable size
+	[r,c]   = size(number);
+	if(r > 1 || c > 1)
+		error('number Variable must be number not a matrix!');
+	end
+
+	exponent = floor(log10(abs(number)));
+	mantissa = number/10^exponent;
+	if(~endline) %If not endline
+		if(abs(exponent)<=3)
+			fprintf(' $%.4f$ &',number);
+		else
+			fprintf(' $%.2f \\cdot 10^{%d}$ &',mantissa,exponent);
+		end
+	else %If in endline print latex array endline
+		if(abs(exponent)<=3)
+			fprintf(' $%.4f$ \\\\',number);
+		else
+			fprintf(' $%.2f \\cdot 10^{%d}$ \\\\',mantissa,exponent);
+		end
+	end
+
+	end %End of internal function
+
+end %End of global function
